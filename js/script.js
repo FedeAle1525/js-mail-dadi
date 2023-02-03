5f0b4dd52db542f8523b3ebb2765a163f357734b
FedeAle1525 <fedeale.1525@gmail.com>
2023-02-03 16:55:16 +0100

    EDIT "Esercizio 1 - Mail" + STYLE

--- js/script.js
+++ js/script.js
@@ -4,40 +4,47 @@
 let arrayMail = ["fede@gmail.com", "ale12@hotmail.it", "andrew18@gmail.com", "erik-29@hotmail.it", "smemo25@libero.it"];
 console.log("Array:", arrayMail);
 
-// 2. Chiedere a Utente la Mail tramite prompt
-const mailUtente = prompt("Inserisci la tua mail ;)");
-console.log("Utente: ", mailUtente);
+// 2. Recupero elemento "pulsante" da HTML
+const pulsanteMailEl = document.querySelector(".container button.btn-success");
 
-// 3. Controllare se la mail dell'Utente è presente nell'Array.
-let messaggio = "no";
+// 3. Chiedere a Utente la Mail tramite prompt dopo aver cliccato il pulsante
+pulsanteMailEl.addEventListener("click", function() {
 
-for(let i = 0; i < arrayMail.length; i++){
-
-  // console.log("MailUtente: ", mailUtente, typeof mailUtente);
-  // console.log("ArrayMail: ", arrayMail[i], typeof arrayMail[i]);
-
-  if (mailUtente === arrayMail[i]){
-    messaggio = "si";
+  const mailUtente = prompt("Inserisci la tua mail ;)");
+  console.log("Utente: ", mailUtente);
+  
+  // 4. Controllare se la mail dell'Utente è presente nell'Array.
+  let messaggio = "no";
+  
+  for(let i = 0; i < arrayMail.length; i++){
+  
+    // console.log("MailUtente: ", mailUtente, typeof mailUtente);
+    // console.log("ArrayMail: ", arrayMail[i], typeof arrayMail[i]);
+  
+    if (mailUtente === arrayMail[i]){
+      messaggio = "si";
+    }
+  
+  }
+  
+  // 5. Stampare un messaggio con esito ricerca
+  if (messaggio === "si"){
+    document.getElementById("mail").innerHTML = "La tua email è già presente!" + " " + '<i class="fa-regular fa-face-laugh-beam"></i>';
+  } else {
+    document.getElementById("mail").innerHTML = "La tua email non è presente!" + " " + '<i class="fa-regular fa-face-frown"></i>';
   }
 
-}
-
-// 4. Stampare un messaggio con esito ricerca
-if (messaggio === "si"){
-  document.getElementById("mail").innerHTML = "La tua email è già presente!";
-} else {
-  document.getElementById("mail").innerHTML = "La tua email non è presente!";
-}
+});
 
 
 // ESERCIZIO 2 - DADI
 
 // 1. Recuperare elemento "pulsante" da HTML
-const pulsanteEl = document.querySelector(".container button");
+const pulsanteDadiEl = document.querySelector(".container button.btn-warning");
 // console.log(pulsanteEl);
 
 // 2. Generare un numero casuale (da 1 a 6) per Utente e PC al "click" del pulsante
-pulsanteEl.addEventListener("click", function() {
+pulsanteDadiEl.addEventListener("click", function() {
   const numeroUtente = Math.floor(Math.random() * (6 - 1 + 1) + 1);
   const numeroPC = Math.floor(Math.random() * (6 - 1 + 1) + 1);
 
@@ -49,10 +56,12 @@ pulsanteEl.addEventListener("click", function() {
     document.getElementById("esito").innerHTML = "Hai Vinto!" + " " + '<i class="fa-regular fa-face-laugh-beam"></i>';
   }
 
+  // 4. Se numeroUtente < numeroPC ----> L'Utente perde!
   if(numeroUtente < numeroPC){
     document.getElementById("esito").innerHTML = "Hai Perso!" + " " + '<i class="fa-regular fa-face-frown"></i>';
   }
 
+  // 5. Se i numeri sono uguali è un pareggio.
   if (numeroUtente === numeroPC){
     document.getElementById("esito").innerHTML = "Pareggio!" + " " + '<i class="fa-solid fa-xmark"></i>';
   }
